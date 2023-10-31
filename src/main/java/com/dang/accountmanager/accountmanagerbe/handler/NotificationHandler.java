a8de2281644f1ed52eb7e1cc43f634de81534c30
thomasdang1996 <thomasdang1996@gmail.com>
2023-10-31 22:10:29 +0100

    master: edited README

--- src/main/java/com/dang/accountmanager/accountmanagerbe/handler/NotificationHandler.java
+++ src/main/java/com/dang/accountmanager/accountmanagerbe/handler/NotificationHandler.java
@@ -4,7 +4,6 @@ import avrogenerated.accountmanager.CreateAccountPayload;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-
 /**
  * Class responsible for handling messages
  */
